6c74e61e673387e0c73229a98d5a403a854ced23
SantiniFr <rafaelfarias.santini14@gmail.com>
2022-03-07 21:45:35 -0300

    fix:Arrumando nome de variável e corrigindo logica do numero não ser divisivel por zero

--- ExerciciosAula1/script.js
+++ ExerciciosAula1/script.js
@@ -6,39 +6,36 @@
 
 */
 
-let answer = prompt('Digite algo: ')
+  let answer = prompt('Digite algo: ')
 
-if (answer === 'Sim' || answer === 'SIM') {
-  alert("Parabéns")
-} else if (answer === 'Não') {
-  answer = prompt('Digite novamente: ')
-  alert(answer)
-} else {
-  confirm('Você tem noções dos seus atos?')
-}
+  if (answer === 'Sim' || answer === 'SIM') {
+    alert("Parabéns")
+  } else if (answer === 'Não') {
+    answer = prompt('Digite novamente: ')
+    alert(answer)
+  } else {
+    confirm('Você tem noções dos seus atos?')
+  }
 
-// /*
-//  Crie um programa que solicita para o usuário dois valores de texto, nome do cliente e nome do atendente;
-//  Exiba um alerta para o usuário com a mensagem "Olá cliente eu me chamo atendente, em que posso ajudar?" (String interpolation)
+  /*
+   2)Crie um programa que solicita para o usuário dois valores de texto, nome do cliente e nome do atendente;
+   Exiba um alerta para o usuário com a mensagem "Olá cliente eu me chamo atendente, em que posso ajudar?" (String interpolation)
+  */
 
-// */
+  const clientName = prompt('Qual o nome do cliente? ');
+  const attendantName = prompt('Qual o nome do atendente?');
 
-const clientName = prompt('Qual o nome do cliente? ');
-const attendantName = prompt('Qual o nome do atendente?');
-
-alert(`Olá ${clientName} eu me chamo ${attendantName}, em que posso ajudar? `)
+  alert(`Olá ${clientName} eu me chamo ${attendantName}, em que posso ajudar? `)
 
 
-// /*
-
-// 3) Crie um programa que possui 3 variáveis do tipo Boolean (true / false);
-//    nomes das variáveis: isFriday, isTwoGreaterThanFour e isValueEmpty;
-//    Atribua à variável isFriday o resultado de uma pergunta feita para o usuário "Hoje é sexta-feira?";
-//    Atribua à variável isTwoGreaterThanFour o resultado da comparação de "2 maior que 4";
-
-//    Para a variável isValueEmpty você precisa solicitar uma informação para o usuário e armazenar na variável a verificação se essa informação é:
-//    null, undefined ou um texto vazio ('')
-// */
+ /*
+ 3) Crie um programa que possui 3 variáveis do tipo Boolean (true / false);
+    nomes das variáveis: isFriday, isTwoGreaterThanFour e isValueEmpty;
+    Atribua à variável isFriday o resultado de uma pergunta feita para o usuário "Hoje é sexta-feira?";
+    Atribua à variável isTwoGreaterThanFour o resultado da comparação de "2 maior que 4";
+    Para a variável isValueEmpty você precisa solicitar uma informação para o usuário e armazenar na variável a verificação se essa informação é:
+   null, undefined ou um texto vazio ('')
+*/
 
 
 let isFriday = confirm('Hoje é sexta-feira?');
@@ -54,43 +51,43 @@ let isValueEmpty = answerUser === null || answerUser === undefined || answerUser
 
  */
 
-let num1 = parseFloat(prompt('Digite o primeiro número: '));
+  let num1 = parseFloat(prompt('Digite o primeiro número: '));
 
-let operation = prompt('Digite uma operação: ');
+  let operation = prompt('Digite uma operação: ');
 
-let num2 = parseFloat(prompt('Digite o segundo número: '));
+  let num2 = parseFloat(prompt('Digite o segundo número: '));
 
-const isNumber = !isNaN(num1) && !isNaN(num2);
+  const isNumber = !isNaN(num1) && !isNaN(num2);
 
-const isNumberInfinity = operation === '/' && num1 / num2 === Infinity;
+  const isNumberInfinity = operation === '/' && num1 / num2 === Infinity || operation === '/' && num2 / num1 === Infinity;
 
-if (!isNumber) {
-  alert('Digite um valor válido');
-}
-
-if (isNumberInfinity) {
-  alert('Não pode ser divisivel por zero');
-}
-
-if (isNumber && !isNumberInfinity === true) {
-  switch (operation) {
-    case '+':
-      alert(`Os numeros somados são ${num1 + num2}`);
-      break;
-    case '-':
-      alert(`Os numeros subtraidos são ${num1 - num2}`);
-      break;
-    case '*':
-      alert(`Os numeros multiplicados são ${num1 * num2}`);
-      break;
-    case '/':
-      alert(`Os numeros divididos são ${num1 / num2}`);
-      break;
-    default:
-      alert('Operação inválida');
-      break;
+  if (!isNumber) {
+    alert('Digite um valor válido');
   }
-}
+
+  if (isNumberInfinity) {
+    alert('Não pode ser divisivel por zero');
+  }
+
+  if (isNumber && !isNumberInfinity) {
+    switch (operation) {
+      case '+':
+        alert(`Os numeros somados são ${num1 + num2}`);
+        break;
+      case '-':
+        alert(`Os numeros subtraidos são ${num1 - num2}`);
+        break;
+      case '*':
+        alert(`Os numeros multiplicados são ${num1 * num2}`);
+        break;
+      case '/':
+        alert(`Os numeros divididos são ${num1 / num2}`);
+        break;
+      default:
+        alert('Operação inválida');
+        break;
+    }
+  }
 
 
 /**
@@ -100,23 +97,24 @@ if (isNumber && !isNumberInfinity === true) {
    
  */
 
-const T1 = parseFloat(prompt('Digite a primeira nota: '));
-const T2 = parseFloat(prompt('Digite a segunda nota: '));
-const P1 = parseFloat(prompt('Digite a terceira nota: '));
+  const T1 = parseFloat(prompt('Digite a primeira nota: '));
+  const T2 = parseFloat(prompt('Digite a segunda nota: '));
+  const P1 = parseFloat(prompt('Digite a terceira nota: '));
 
-const average = (T1 + T2 + P1) / 3;
+  const average = (T1 + T2 + P1) / 3;
 
-const isNumberInfinity = !isNaN(T1) && !isNaN(T2) && !isNaN(P1)
+  const isNumberInfinity = !isNaN(T1) && !isNaN(T2) && !isNaN(P1)
 
-if (!isNumber) {
-  alert('Digite um numero')
-} else if (average < 5) {
-  alert('Reprovado')
-} else if (average >= 5 && average < 7) {
-  alert('Recuperação')
-} else {
-  alert('Parabéns você passou!!!')
-}
+
+  if (!isNumberInfinity) {
+    alert('Digite um numero')
+  } else if (average < 5) {
+    alert('Reprovado')
+  } else if (average >= 5 && average < 7) {
+    alert('Recuperação')
+  } else {
+    alert('Parabéns você passou!!!')
+  }
 
 /*
 6) Crie um programa que solicite para o usuário que escolha uma das opções do menu:
